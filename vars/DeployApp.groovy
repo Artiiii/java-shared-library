0953be787e83b201872aff34b1691f7959eaa79d
Artiiii <31152341+Artiiii@users.noreply.github.com>
2022-07-28 01:10:09 +0530

    Update DeployApp.groovy

--- vars/DeployApp.groovy
+++ vars/DeployApp.groovy
@@ -1,4 +1,11 @@
 def call()
 {
-  cp "target/war.app.war" "C:/Users/Arti Pal/Documents/tools/apache-tomcat-10.0.23-windows-x64/apache-tomcat-10.0.23/webapps"
+  //cp "target/war.app.war" "C:/Users/Arti Pal/Documents/tools/apache-tomcat-10.0.23-windows-x64/apache-tomcat-10.0.23/webapps"
+  
+  fileOperations([fileCopyOperation(
+  excludes: '',
+  flattenFiles: false,
+  includes: 'target/war.app.war',
+  targetLocation: "C:/Users/Arti Pal/Documents/tools/apache-tomcat-10.0.23-windows-x64/apache-tomcat-10.0.23/webapps"
+)]).
 }
