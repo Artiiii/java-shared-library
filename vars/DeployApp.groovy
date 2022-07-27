27a287edd7139e1812f580d973a374fe533c7cea
Artiiii <31152341+Artiiii@users.noreply.github.com>
2022-07-28 01:11:56 +0530

    Update DeployApp.groovy

--- vars/DeployApp.groovy
+++ vars/DeployApp.groovy
@@ -7,5 +7,5 @@ def call()
   flattenFiles: false,
   includes: 'target/war.app.war',
   targetLocation: "C:/Users/Arti Pal/Documents/tools/apache-tomcat-10.0.23-windows-x64/apache-tomcat-10.0.23/webapps"
-)]).
+)])
 }
