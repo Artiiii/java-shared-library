33dbba307068a5718f4603e5174be2c0346bef2b
Artiiii <31152341+Artiiii@users.noreply.github.com>
2022-07-27 20:35:31 +0530

    Update Maven.groovy

--- vars/Maven.groovy
+++ vars/Maven.groovy
@@ -2,35 +2,32 @@ def call(String repoUrl) {
   pipeline {
        agent any
        stages {
-         //stage("Checkout")
-        // {
-        //   git 
-        // }
-        ////   stage("Tools initialization") {
-          //     steps {
-          //         sh "mvn --version"
-          //         sh "java -version"
-          //     }
-          // }
+        
+           stage("Tools initialization") {
+             steps {
+                 sh "mvn --version"
+                 sh "java -version"
+             }
+         }
            stage("Checkout Code") {
                steps {
                    git branch: 'master',
                        url: "${repoUrl}"
                }
            }
-          // stage("Cleaning workspace") {
-          //     steps {
-          //         sh "mvn clean"
-          //     }
-          // }
-          // stage("Running Testcase") {
-          //    steps {
-          //         sh "mvn test"
-          //     }
-          // }
-          // stage("Packing Application") {
-          //     steps {
-          //         sh "mvn package -DskipTests"
-          //     }
-          // }
+           stage("Cleaning workspace") {
+               steps {
+                   sh "mvn clean"
+               }
+           }
+           stage("Running Testcase") {
+              steps {
+                   sh "mvn test"
+               }
+           }
+           stage("Packing Application") {
+               steps {
+                   sh "mvn package -DskipTests"
+               }
+           }
        }
