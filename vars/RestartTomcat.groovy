620bfae2133dc87c13ce836dd5b0d07c79be6daf
Artiiii <31152341+Artiiii@users.noreply.github.com>
2022-07-28 11:00:06 +0530

    Delete RestartTomcat.groovy

--- vars/RestartTomcat.groovy
+++
@@ -1,9 +0,0 @@
-def call()
-{
-  powershell "cd C:/Users/Arti Pal/Documents/tools/apache-tomcat-10.0.23-windows-x64/apache-tomcat-10.0.23/bin"
-  bat "call shutdown.bat"
-  //powershell label: '', script: 'Invoke-Item "C:/Users/Arti Pal/Documents/tools/apache-tomcat-10.0.23-windows-x64/apache-tomcat-10.0.23/bin/shutdown.bat"'
-  // "C://Users//Arti Pal//Documents//tools//apache-tomcat-10.0.23-windows-x64//apache-tomcat-10.0.23//bin//startup.bat"
-  powershell label: "" , script: "Start-Sleep 4"
-  bat "start C:/Windows/System32/cmd.exe 'C:/Users/Arti Pal/Documents/tools/apache-tomcat-10.0.23-windows-x64/apache-tomcat-10.0.23/bin/startup.bat'"
-}
