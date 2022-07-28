f192743118b9346f1e7d375bf70ebbf3f54f922e
Artiiii <31152341+Artiiii@users.noreply.github.com>
2022-07-28 09:42:43 +0530

    Update DeployApp.groovy

--- vars/DeployApp.groovy
+++ vars/DeployApp.groovy
@@ -1,9 +1,4 @@
 def call()
 {
-  //cp "target/war.app.war" "C:/Users/Arti Pal/Documents/tools/apache-tomcat-10.0.23-windows-x64/apache-tomcat-10.0.23/webapps"
-  
-//bat label: '', script: 'xcopy "target\war.app.war" "C:\Users\Arti Pal\Documents\tools\apache-tomcat-10.0.23-windows-x64\apache-tomcat-10.0.23\webapps"'
- // bat label: '', script: 'xcopy "H:\yes.txt" "H:\tools" \Y'
-//powershell label: '', script: 'Copy-Item "H:/yes.txt" "H:/tools" -Force -Verbose'
 powershell  label: '', script: 'Copy-Item "target/war.app.war" "C:/Users/Arti Pal/Documents/tools/apache-tomcat-10.0.23-windows-x64/apache-tomcat-10.0.23/webapps" -Force -Verbose'
 }
