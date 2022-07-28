9ee1d173178b126b9a9945c34d356732e233a5e7
Artiiii <31152341+Artiiii@users.noreply.github.com>
2022-07-28 09:59:13 +0530

    Update RestartTomcatTest.groovy

--- vars/RestartTomcatTest.groovy
+++ vars/RestartTomcatTest.groovy
@@ -1,11 +1,4 @@
 def call()
 {
-  bat """
-  cd 'C:\Users\Arti Pal\Documents\tools\apache-tomcat-10.0.23-windows-x64\apache-tomcat-10.0.23\bin'
-  shutdown
-  """
-  bat """
-  cd 'C:\Users\Arti Pal\Documents\tools\apache-tomcat-10.0.23-windows-x64\apache-tomcat-10.0.23\bin'
-  startup
-  """
+  bat "H:\ar.sample.java.war.app\copy.bat"
 }
