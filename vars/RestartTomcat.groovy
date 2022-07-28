4db633b614b0384cf8504a92c98e63d3ba6603b9
Artiiii <31152341+Artiiii@users.noreply.github.com>
2022-07-28 10:46:02 +0530

    Update RestartTomcat.groovy

--- vars/RestartTomcat.groovy
+++ vars/RestartTomcat.groovy
@@ -1,7 +1,7 @@
 def call()
 {
  // bat "start C:/Windows/System32/cmd.exe 'C:/Users/Arti Pal/Documents/tools/apache-tomcat-10.0.23-windows-x64/apache-tomcat-10.0.23/bin/shutdown.bat'"
-  powershell label: '', script: 'Start-Process -FilePath "C:/Users/Arti Pal/Documents/tools/apache-tomcat-10.0.23-windows-x64/apache-tomcat-10.0.23/bin/shutdown.bat"'
+  powershell label: '', script: 'Invoke-Item "C:/Users/Arti Pal/Documents/tools/apache-tomcat-10.0.23-windows-x64/apache-tomcat-10.0.23/bin/shutdown.bat"'
   // "C://Users//Arti Pal//Documents//tools//apache-tomcat-10.0.23-windows-x64//apache-tomcat-10.0.23//bin//startup.bat"
   powershell label: "" , script: "Start-Sleep 4"
   bat "start C:/Windows/System32/cmd.exe 'C:/Users/Arti Pal/Documents/tools/apache-tomcat-10.0.23-windows-x64/apache-tomcat-10.0.23/bin/startup.bat'"
