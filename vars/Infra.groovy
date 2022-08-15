63d62194aa51ee6ec54eda43df9c8754354cfba8
Artiiii <31152341+Artiiii@users.noreply.github.com>
2022-08-15 18:01:29 +0530

    Update Infra.groovy

--- vars/Infra.groovy
+++ vars/Infra.groovy
@@ -1,20 +1,20 @@
 def init()
 {
-  sh label: '', script: """
+  powershell label: '', script: """
   cd infra
   terraform init -input=false
   """
 }
 def plan()
 {
-  sh label: '', script: """
+  powershell label: '', script: """
   cd infra
   terraform plan -out=tfplan -input=false
   """
 }
 def apply()
 {
-  sh label: '', script: """
+  powershell label: '', script: """
   cd infra
   terraform apply -input=false tfplan
   """
