8e7f1a91cbd1cedddb7554341d03339e6cf1aa3a
Artiiii <31152341+Artiiii@users.noreply.github.com>
2022-08-15 18:08:47 +0530

    Update Infra.groovy

--- vars/Infra.groovy
+++ vars/Infra.groovy
@@ -1,7 +1,7 @@
 def call()
 {
   bat label: '', script: """
-  terraform -chdir infra
+  terraform -chdir=infra
   terraform init -input=false
   terraform plan -out=tfplan -input=false
   terraform apply -input=false tfplan
