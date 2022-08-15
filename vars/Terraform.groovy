fc1e349d2df81938483bfc2f79ce9ab77e929340
Artiiii <31152341+Artiiii@users.noreply.github.com>
2022-08-15 16:53:26 +0530

    Update Terraform.groovy

--- vars/Terraform.groovy
+++ vars/Terraform.groovy
@@ -1,12 +1,21 @@
 def init()
 {
-  powershell label: '', script: 'terraform init -input=false'
+  powershell label: '', script: """
+  cd infra
+  terraform init -input=false
+  """
 }
 def plan()
 {
-  powershell label: '', script: 'terraform plan -out=tfplan -input=false'
+  powershell label: '', script: """
+  cd infra
+  terraform plan -out=tfplan -input=false
+  """
 }
 def apply()
 {
-  powershell label: '', script: 'terraform apply -input=false tfplan'
+  powershell label: '', script: """
+  cd infra
+  terraform apply -input=false tfplan
+  """
 }
