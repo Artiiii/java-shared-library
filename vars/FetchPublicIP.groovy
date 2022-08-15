5b811f170e95b5b97a1b31aa06a80054099aef96
Artiiii <31152341+Artiiii@users.noreply.github.com>
2022-08-15 21:26:20 +0530

    Update FetchPublicIP.groovy

--- vars/FetchPublicIP.groovy
+++ vars/FetchPublicIP.groovy
@@ -2,10 +2,10 @@ def call() {
  
   powershell label: '', script: """
         az login --service-principal -u $azcon_CLIENT_ID -p $azcon_CLIENT_SECRET -t $azcon_TENANT_ID'
-        az vm list-ip-addresses --resource-group ranjith --name arti-java-vm > fetchpublicip.json
-        Get-Content fetchpublicip.json
-        $publicip = Get-Content fetchpublicip.json | ConvertFrom-Json
-        $publicip.virtualMachine.network.publicIpAddresses.ipAddress > publicip.txt
+        az vm list-ip-addresses --resource-group ranjith --name arti-java-vm #> fetchpublicip.json
+       # Get-Content fetchpublicip.json
+       # $publicip = Get-Content fetchpublicip.json | ConvertFrom-Json
+        #$publicip.virtualMachine.network.publicIpAddresses.ipAddress > publicip.txt
         """
         
 }
