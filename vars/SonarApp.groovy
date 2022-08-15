5e17a54c93f7e0b10932dc89f03c965a71346aac
Artiiii <31152341+Artiiii@users.noreply.github.com>
2022-08-15 14:51:05 +0530

    Update SonarApp.groovy

--- vars/SonarApp.groovy
+++ vars/SonarApp.groovy
@@ -1,4 +1,4 @@
 def call(login)
 {
-bat label: '', script: 'H:\\apache-maven-3.8.6-bin\\apache-maven-3.8.6\\bin\\mvn.cmd clean verify sonar:sonar -Dsonar.host.url=http://localhost:9000 -Dsonar.login=${login}'
+bat label: '', script: 'H:\\apache-maven-3.8.6-bin\\apache-maven-3.8.6\\bin\\mvn.cmd clean verify sonar:sonar -Dsonar.host.url=http://localhost:9000 -Dsonar.login=$login'
 }
