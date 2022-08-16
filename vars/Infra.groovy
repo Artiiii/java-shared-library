93672bf0f35850696b64ff9615539241472f638a
Artiiii <31152341+Artiiii@users.noreply.github.com>
2022-08-16 16:09:11 +0530

    Update Infra.groovy

--- vars/Infra.groovy
+++ vars/Infra.groovy
@@ -5,8 +5,10 @@ def call()
   set ARM_CLIENT_SECRET=$azcon_CLIENT_SECRET
   set ARM_SUBSCRIPTION_ID=$azcon_SUBSCRIPTION_ID
   set ARM_TENANT_ID=$azcon_TENANT_ID
-  terraform -chdir=infra init -input=false
-  terraform -chdir=infra plan -out=tfplan -input=false
-  terraform -chdir=infra apply -input=false tfplan
+  terraform -chdir=infra destroy   
   """
 }
+
+//terraform -chdir=infra init -input=false
+//  terraform -chdir=infra plan -out=tfplan -input=false
+//  terraform -chdir=infra apply -input=false tfplan
