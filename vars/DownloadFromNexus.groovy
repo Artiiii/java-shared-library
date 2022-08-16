77dca75cdcc6c8069f7a69699ed3cd4f973c82b8
Artiiii <31152341+Artiiii@users.noreply.github.com>
2022-08-16 18:00:35 +0530

    Update DownloadFromNexus.groovy

--- vars/DownloadFromNexus.groovy
+++ vars/DownloadFromNexus.groovy
@@ -1,4 +1,4 @@
 def call()
 {
-curl -L -X GET "http://localhost:8081/service/rest/v1/search/assets/download?sort=version&repository=maven-snapshots&maven.groupId=com.javawebtutor&maven.artifactId=LoginWebApp&maven.extension=war"  -H "accept: application/json" --output LoginWebApp.war
+Invoke-WebRequest -Method GET "http://localhost:8081/service/rest/v1/search/assets/download?sort=version&repository=maven-snapshots&maven.groupId=com.javawebtutor&maven.artifactId=LoginWebApp&maven.extension=war"  -ContentType "application/json" -OutFile LoginWebApp.war
 }
