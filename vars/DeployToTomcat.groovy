c3e2d3d0777d6d1141502276ffbc9859c314b3ff
Artiiii <31152341+Artiiii@users.noreply.github.com>
2022-08-16 20:34:09 +0530

    Update DeployToTomcat.groovy

--- vars/DeployToTomcat.groovy
+++ vars/DeployToTomcat.groovy
@@ -2,6 +2,6 @@ def call()
 {
 powershell label: '', script: '''
 $vm = Get-Content output.txt
-scp ./LoginWebApp.war arti@$vm:/opt/tomcat/9_37/webapps
+scp ./LoginWebApp.war arti@${vm}:/opt/tomcat/9_37/webapps
 '''
 }
