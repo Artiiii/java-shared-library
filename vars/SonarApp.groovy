ec94e03711d2b5bf1587217cba7387f76e8934a9
Artiiii <31152341+Artiiii@users.noreply.github.com>
2022-08-16 21:42:00 +0530

    Update SonarApp.groovy

--- vars/SonarApp.groovy
+++ vars/SonarApp.groovy
@@ -1,4 +1,4 @@
 def call()
 {
-bat label: '', script: 'mvn clean verify sonar:sonar -Dsonar.projectKey=ar.sample.java.war.app -Dsonar.host.url=http://localhost:9000 -Dsonar.login=sqp_8d416d2a6afd97b608af543e2f6dc823ff237754'
+bat label: '', script: """mvn clean verify sonar:sonar -Dsonar.host.url=http://localhost:9000 -Dsonar.login=${login}"""
 }
