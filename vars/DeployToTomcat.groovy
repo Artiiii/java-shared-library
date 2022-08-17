1d51cd0c408d2d7131057b58066bcef37037191b
Artiiii <31152341+Artiiii@users.noreply.github.com>
2022-08-17 10:04:38 +0530

    Update DeployToTomcat.groovy

--- vars/DeployToTomcat.groovy
+++ vars/DeployToTomcat.groovy
@@ -1,12 +1,11 @@
-def remote = [:]
+def call()
+{
+remote = [:]
 remote.name = 'azurevm'
 //remote.host = '40.85.162.228'
 remote.user = 'arti'
 remote.password = 'Arti123'
-remote.allowAnyHosts = true
-  
-def call()
-{
+remote.allowAnyHosts = tru
 remote.host = powershell("Get-content output.txt")
 sshPut remote: remote, from: 'LoginWebApp.war', into: '/opt/tomcat/9_37/webapps'
 }
